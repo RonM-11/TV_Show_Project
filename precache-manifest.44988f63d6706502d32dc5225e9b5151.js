2a451707cba2eb9a8bca8c184676b5a2cfc36733
Ron <rongalron@gmail.com>
2020-06-03 02:19:12 +0300

    Updates

---
+++ precache-manifest.44988f63d6706502d32dc5225e9b5151.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce4794d7aba2e219cf6e538d650defe",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b9344a3be66b8bf09266",
+    "url": "/my-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "73a9ef5d242cb6227e98",
+    "url": "/my-app/static/css/main.926e4029.chunk.css"
+  },
+  {
+    "revision": "b9344a3be66b8bf09266",
+    "url": "/my-app/static/js/2.85a6fae1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.85a6fae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a9ef5d242cb6227e98",
+    "url": "/my-app/static/js/main.fdc971bf.chunk.js"
+  },
+  {
+    "revision": "99cce06d774f8161a5bf",
+    "url": "/my-app/static/js/runtime-main.52ccf6d9.js"
+  }
+]);
